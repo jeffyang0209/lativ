c2c18f49f59d7e126c91ad25d485f0be23e1f369
WINDEV2010EVAL\User <s>
2020-11-04 05:52:33 -0800

    CRUD

--- Lativ/Controllers/ValuesController.cs
+++ Lativ/Controllers/ValuesController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Lativ.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Lativ.Controllers
@@ -10,30 +13,71 @@ namespace Lativ.Controllers
     public class ValuesController : ApiController
     {
         // GET api/<controller>
-        public IEnumerable<string> Get()
+        public async Task<List<Table>> Get()
         {
-            return new string[] { "value1", "value2" };
+            using (var x = new DataEntities1())
+            {
+                var result = await x.Table.ToListAsync();
+                return result;
+            }
         }
 
         // GET api/<controller>/5
-        public string Get(int id)
+        public async Task<Table> Get(int id)
         {
-            return "value";
+            using (var x = new DataEntities1())
+            {
+                var result = await x.Table.FirstOrDefaultAsync(_ => _.Id == id);
+                return result;
+            }
         }
 
         // POST api/<controller>
-        public void Post([FromBody] string value)
+        public async Task<int> Post([FromBody] Table model)
         {
+            using (var x = new DataEntities1())
+            {
+                x.Table.Add(new Table
+                {
+                    Title = model.Title,
+                    Content = model.Content
+                });
+                var result = await x.SaveChangesAsync();
+                return result;
+            }
         }
 
         // PUT api/<controller>/5
-        public void Put(int id, [FromBody] string value)
+        public async Task Put([FromBody] Table model)
         {
+            using (var x = new DataEntities1())
+            {
+                var table = await x.Table.FirstOrDefaultAsync(_ => _.Id == model.Id);
+                if (table == null)
+                {
+                    return;
+                }
+
+                table.Content = model.Content;
+                table.Title = model.Title;
+
+                await x.SaveChangesAsync();
+            }
         }
 
         // DELETE api/<controller>/5
-        public void Delete(int id)
+        public async Task Delete(int id)
         {
+            using (var x = new DataEntities1())
+            {
+                var table = await x.Table.FirstOrDefaultAsync(_ => _.Id == id);
+                if (table != null)
+                {
+                    x.Table.Remove(table);
+                    await x.SaveChangesAsync();
+
+                }
+            }
         }
     }
 }
